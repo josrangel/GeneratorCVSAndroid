33ea4c5c4b962ab4493fcaf53173212edebb1cef
Josue <josrangel@hotmail.com>
2021-05-16 19:40:30 -0500

    feat: Add message of File generate

--- app/src/main/java/com/josrangel/generatorcsv/MainActivity.java
+++ app/src/main/java/com/josrangel/generatorcsv/MainActivity.java
@@ -3,6 +3,7 @@ package com.josrangel.generatorcsv;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.Toast;
 
 import com.opencsv.CSVWriter;
 
@@ -14,33 +15,37 @@ import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private String csv;
+    private String csvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        csv = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyCsvFile.csv"); // Here csv file name is MyCsvFile.csv
+        csvName = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyCsvFile.csv"); // Here csv file name is MyCsvFile.csv
 
     }
 
     public void generateCsv(View v){
         CSVWriter writer = null;
         try {
-            writer = new CSVWriter(new FileWriter(csv));
+            writer = new CSVWriter(new FileWriter(csvName));
 
             List<String[]> data = new ArrayList<>();
             data.add(new String[]{"Country", "Capital"});
             data.add(new String[]{"India", "New Delhi"});
             data.add(new String[]{"United States", "Washington D.C"});
             data.add(new String[]{"Germany", "Berlin"});
-
             writer.writeAll(data); // data is adding to csv
-
             writer.close();
+            showMessage("Your file was generate correctly in " + csvName);
             //callRead();
         } catch (IOException e) {
             e.printStackTrace();
+            showMessage("Error: " + e.toString());
         }
+    }
+
+    private void showMessage(String text){
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     }
 
 }
